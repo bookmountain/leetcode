cbbf9908f1db985f4e1c0b0e378c2885d17b49cd
Book Chang <bookmountain0222@gmail.com>
2023-08-06 15:27:33 +0800

    Update: add 2-D dp problems

--- problems/2-D_dynamic_programming/1143.longestCommonSubsequence.js
+++ problems/2-D_dynamic_programming/1143.longestCommonSubsequence.js
@@ -1,41 +1,17 @@
 var longestCommonSubsequence = function (text1, text2) {
-    const m = text1.length;
-    const n = text2.length;
-
-    // Create a 2D array to store the lengths of common subsequences
-    const dp = new Array(m + 1);
-    for (let i = 0; i <= m; i++) {
-        dp[i] = new Array(n + 1).fill(0);
-    }
-
-    // Fill the 2D array with the lengths of common subsequences
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (text1[i - 1] === text2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
+    const dp = new Array(text1.length + 1).fill(0).map(() => new Array(text2.length + 1).fill(0));
+    for (let i = text1.length - 1; i >= 0; i--) {
+        for (let j = text2.length - 1; j >= 0; j--) {
+            if (text1[i] === text2[j]) {
+                dp[i][j] = dp[i + 1][j + 1] + 1;
             } else {
-                dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
             }
+            console.log(dp, i, j);
         }
     }
-
-    // Retrieve the longest common subsequence
-    let i = m;
-    let j = n;
-    const result = [];
-    while (i > 0 && j > 0) {
-        if (text1[i - 1] === text2[j - 1]) {
-            result.unshift(text1[i - 1]);
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-
-    return result.join("").length;
+    console.table(dp);
+    return dp[0][0];
 };
 
 console.log(longestCommonSubsequence("abcde", "ace"));
