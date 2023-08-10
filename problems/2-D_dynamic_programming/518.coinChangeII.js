b70ec878ce7d2cac2ae027367aaeca51b663ba18
Book Chang <bookmountain0222@gmail.com>
2023-08-11 00:14:18 +0800

    Update: add 2-D dp problems

--- problems/2-D_dynamic_programming/518.coinChangeII.js
+++ problems/2-D_dynamic_programming/518.coinChangeII.js
@@ -1,34 +1,35 @@
 var change = function (amount, coins) {
-    const dp = new Array(amount + 1).fill(0);
-    dp[0] = 1;
-    for (const coin of coins) {
-        for (let i = 1; i < dp.length; i++) {
-            if (i >= coin) {
-                dp[i] += dp[i - coin];
-            }
-        }
-        console.log(dp);
-    }
-    return dp[amount];
+    // const dp = new Array(amount + 1).fill(0);
+    // dp[0] = 1;
+    // for (const coin of coins) {
+    //     for (let i = 1; i < dp.length; i++) {
+    //         if (i >= coin) {
+    //             dp[i] += dp[i - coin];
+    //         }
+    //     }
+    //     console.log(dp);
+    // }
+    // return dp[amount];
 
     // DYNAMIC PROGRAMMING
     // Time: O(n*m)
     // Memory: O(n*m)
-    // const dp = Array.from({ length: amount + 1 }, () => Array(coins.length + 1).fill(0));
-    // for (let i = 0; i <= coins.length; i++) {
-    //     dp[0][i] = 1;
-    // }
+    const dp = new Array(amount + 1).fill(0).map(() => new Array(coins.length + 1).fill(0));
+    for (let i = 0; i <= coins.length; i++) {
+        dp[0][i] = 1;
+    }
 
-    // for (let a = 1; a <= amount; a++) {
-    //     for (let i = coins.length - 1; i >= 0; i--) {
-    //         dp[a][i] = dp[a][i + 1];
-    //         if (a - coins[i] >= 0) {
-    //             dp[a][i] += dp[a - coins[i]][i];
-    //         }
-    //     }
-    // }
-    // console.table(dp);
-    // return dp[amount][0];
+    for (let i = coins.length - 1; i >= 0; i--) {
+        const coin = coins[i];
+        for (let a = 1; a <= amount; a++) {
+            dp[a][i] = dp[a][i + 1];
+            if (a - coin >= 0) {
+                dp[a][i] += dp[a - coin][i];
+            }
+        }
+        console.table(dp);
+    }
+    return dp[amount][0];
 };
 
 console.log(change(5, [1, 2, 5])); // 4
